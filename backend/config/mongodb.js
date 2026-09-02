612ea4a9f02b32096da9d5db8b3f893951888f64
Pratik <pratikdatir2004@gmail.com>
2026-09-02 19:57:25 +0530

    Fix MongoDB connection for Vercel

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -2,11 +2,14 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
+    if (mongoose.connection.readyState === 1) {
+      return;
+    }
+
     await mongoose.connect(process.env.MONGO_URI);
     console.log("Database Connected");
   } catch (error) {
-    console.error("Database connection error:", error);
-    process.exit(1);
+    console.error("Database connection error:", error.message);
   }
 };
 
